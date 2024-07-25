a6b63715736c5dfa8f6f67431b932e7c37af80ac
morteza <mori2002sa@gmail.com>
2024-07-25 17:05:14 +0330

    Store data in locale storage

--- src/store/mainStore.ts
+++ src/store/mainStore.ts
@@ -1,16 +1,20 @@
 import { createUserSlice } from "@/store/userSlice";
 import { TMainStore } from "@/types/store";
 import { create } from "zustand";
-import { subscribeWithSelector } from "zustand/middleware";
+import { devtools, persist, subscribeWithSelector } from "zustand/middleware";
 import { immer } from "zustand/middleware/immer";
 import { createCartSlice } from "./cartSlice";
 
 export const useMainStore = create<TMainStore>()(
-  subscribeWithSelector(
-    immer((...a) => ({
-      ...createUserSlice(...a),
-      ...createCartSlice(...a),
-    }))
-
+  devtools(
+    persist(
+      subscribeWithSelector(
+        immer((...a) => ({
+          ...createUserSlice(...a),
+          ...createCartSlice(...a),
+        }))
+      ),
+      { name: "product-storage" }
+    )
   )
 );
